f27cbf6c668f469ec520cf8a01ca625faeb29d94
Bader63 <c.baderamg@icloud.com>
2022-11-01 22:35:22 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function BMI(weight, height) {
+  let result = weight / (height * height);
+  return result;
+}
 
+console.log(BMI(70, 108));
+function status(bodymass) {
+  if (bodymass < 18.5) {
+    return "لديق نقص في الوزن";
+  } else if (bodymass >= 18.5 || bodymass <= 225) {
+    return "وزنك صحي";
+  }
+}
+
+function calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+
+  let bmi_value = BMI(weight, height);
+  let desc = Status(bmi_value);
+  let box = document.getElementById("result");
+  box.innerText = bmi_value + " == " + desc;
+}
